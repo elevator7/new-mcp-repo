1cd0ef68794f370efb1da2a9bb8f36a6bedead0a
Dhruv Patel <145609677+elevator7@users.noreply.github.com>
2025-05-26 12:47:14 -0400

    Switch to HashRouter for GitHub Pages compatibility (fix blank page issue)

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { HashRouter } from 'react-router-dom';
 
 const theme = createTheme({
   palette: {
@@ -26,7 +27,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>,
 );
